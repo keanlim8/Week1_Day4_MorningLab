2b640340ff8319c7d32276d65e964c67b3fcffe2
Kean Lim <keanlim58@gmail.com>
2020-11-18 21:24:43 -0500

    Add add2numbers.js file

---
+++ AddingTwoNumbers.js
@@ -0,0 +1,17 @@
+var num1, num2, num3, num4, num5, num6, num7, num8, num9, num10 ;
+num1=4;
+num2=10;
+num3=22;
+num4=-30;
+num5=55;
+num6=68;
+num7=7;
+num8=-22;
+num9=9;
+num10=100;
+var sum1 = (num1 + num2 + num3 + num4 + num5) / 5;
+var sum2 = (num6 + num7 + num8 + num9 + num10) / 5;
+var sum = (sum1 + sum2) / 2;
+console.log(sum1);
+console.log(sum2);
+console.log(sum);
